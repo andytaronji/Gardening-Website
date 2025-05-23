bbe44a799e7d2bb35daa184783438ad5808d791c
Gardening Thyme <services@gardeningthyme.com>
2025-05-23 17:19:47 -0400

    New google tacking

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!-- Google Tag Manager -->
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WN7LXHF8');
+              <!-- End Google Tag Manager -->
+            `
+          }}
+        />
+        
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11312192738"></script>
         <script
@@ -65,6 +80,15 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WN7LXHF8"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `
+          }}
+        />
         <Preload />
         <Navigation />
         <main>{children}</main>
